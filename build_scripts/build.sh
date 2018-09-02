b619746d233e037669b6e51bbd75d864a117373e
Dario Vianello <dario.vianello@worldpay.com>
2018-09-02 20:56:43 +0200

    Test workflows

---
+++ build_scripts/build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+BINARIES_FOLDER="binaries"
+
+echo "
+###
+## Build binaries
+###"
+
+gox -os="linux darwin" -arch="amd64" -output="${BINARIES_FOLDER}/vault-iam-request_{{.OS}}-{{.Arch}}"
+
+
+echo "
+###
+## UPX binaries
+###"
+upx "${BINARIES_FOLDER}"/*
+
+
+
+echo "
+###
+## Tar everything up
+###"
+
+cd "${BINARIES_FOLDER}"
+for binary in vault-*; do
+    IFS="_" read -a tokens <<< "${binary}"
+    echo "Compressing ${binary}"
+    mv "${binary}" "${tokens[0]}"
+    tar -czf "${tokens[0]}-${CIRCLE_TAG}-${tokens[1]}.tar.gz" "${tokens[0]}"
+    rm "${tokens[0]}"
+done
+cd ..
+
+
+echo "
+###
+## Upload release to Github
+###
+"
+ghr "${CIRCLE_TAG}" "${BINARIES_FOLDER}/*.tar.gz"
